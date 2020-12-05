e07ed8f96927c59a1e4a92e65cae1a7b2f9cf3de
Madhusudhan Aithal <madhuaithal@Madhus-MacBook-Pro.local>
2020-12-05 11:54:17 -0700

    Made some changes

---
+++ gcp_blob_worker/app.py
@@ -0,0 +1,25 @@
+from kafka import KafkaConsumer, KafkaProducer
+import pickle
+import gcp_utils
+import os
+import uuid
+import json
+
+KAFKA_GCP_BLOB_TOPIC = os.getenv("KAFKA_GCP_BLOB_TOPIC") or 'gcp_blob'
+BUCKET_NAME = os.getenv("GCP_BUCKET_NAME") or 'datacenter_project_bucket'
+
+producer = KafkaProducer(bootstrap_servers='localhost:9092', 
+   key_serializer=lambda m: m.encode('utf-8'),
+   value_serializer=lambda m: m.encode('utf-8'))
+
+consumer = KafkaConsumer(KAFKA_GCP_BLOB_TOPIC, bootstrap_servers='localhost:9092')
+
+for msg in consumer:
+    print('Message key:', msg.key.decode('utf-8'))
+    blob_name_uuid = msg.key.decode('utf-8')
+    gcp_utils.upload_blob_from_string(BUCKET_NAME, msg.value, blob_name_uuid)      
+    
+    producer.send(topic='gcp_blob_response', 
+        key=json.dumps({'uuid': blob_name_uuid}), 
+        value=json.dumps({'success': True}))
+
