4a5ccf864933643fecae099060292b24876e8eff
Madhusudhan Aithal <madhuaithal@Madhus-MacBook-Pro.local>
2020-12-05 00:43:18 -0700

    Passing image byte stream through kafka successfully, and other changes

--- main/app.py
+++ main/app.py
@@ -4,25 +4,33 @@ from werkzeug.utils import secure_filename
 import gcp_utils
 import uuid
 from kafka import KafkaProducer
+import pickle
+import json
 
 app = Flask(__name__)
 
 BUCKET_NAME = 'datacenter_project_bucket'
-producer = KafkaProducer(bootstrap_servers='localhost:9092')
+producer = KafkaProducer(bootstrap_servers='localhost:9092', 
+   # value_serializer=lambda m: m.encode('utf-8'), 
+   key_serializer=lambda m: m.encode('utf-8'))
 
 @app.route('/upload', methods = ['GET', 'POST'])
 def upload_file():
    if request.method == 'POST':
+      destination_blob_name = str(uuid.uuid4())
       f = request.files['file']
       blob_name_uuid = str(uuid.uuid4())
       fin = f.stream.read()
       # temp2 = f.stream.read()
-      # temp = secure_filename(f.filename)
+      filename = secure_filename(f.filename)
+      print("filename:", filename)
       # with open(temp, 'rb') as fin:
       #    print(fin)
-      # print(type(temp))
+      # print(type(temp))     
+
       gcp_utils.upload_blob_from_string(BUCKET_NAME, fin, blob_name_uuid)
-      producer.send('foobar', fin)
+      
+      producer.send(topic='foobar', value=fin, key=str(filename))
       # gcp_utils.detect_document_from_file(fin)
       # f.save(secure_filename(f.filename))
       return 'file uploaded successfully'
--- ocr_worker/app.py
+++ ocr_worker/app.py
@@ -1,7 +1,14 @@
 from kafka import KafkaConsumer
+import pickle
+import gcp_utils
+
 consumer = KafkaConsumer('foobar',bootstrap_servers='localhost:9092')
+
 for msg in consumer:
-    for line in msg:
-        print(line)
+    print(msg.key.decode('utf-8'))
+    gcp_utils.detect_document_from_file(msg.value)
+    # msg_2 = pickle.load(msg.value)
+    # for line in msg_2:
+    #     print(line)
 
 
